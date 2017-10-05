87dd8cf5848f05a33a04a5556f586c8326915523
dastangoo <hamed.dastangoo@gmail.com>
2017-10-05 15:56:33 +0330

    Creating layout

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/jquery-ui.css">
     <link rel="stylesheet" href="css/font-awesome.css">
+    <link rel="stylesheet" href="css/style.css">
 
     <script type="text/javascript" src="js/jquery.js"></script>
     <script type="text/javascript" src="js/jquery-color.js"></script>
@@ -13,7 +14,29 @@
     <script type="text/javascript" src="js/bootstrap.js"></script>
   </head>
   <body>
-    <div class="jumbotron">
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a href="#" class="navbar-brand">Online Shopping</a>
+        </div>
+        <ul class="nav navbar-nav">
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Men</a></li>
+          <li><a href="#">Women</a></li>
+          <li><a href="#">Kids</a></li>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="">Log Out</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3"><div class="col-md-12 single-item">Item 1</div></div>
+        <div class="col-md-3"><div class="col-md-12 single-item">Item 2</div></div>
+        <div class="col-md-3"><div class="col-md-12 single-item">Item 3</div></div>
+        <div class="col-md-3"><div class="col-md-12 single-item">Item 4</div></div>        
+      </div>
     </div>
   </body>
 </html>
