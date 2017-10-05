5a1f506f7c601118f9850d4e6b1a5504e40c24ef
dastangoo <hamed.dastangoo@gmail.com>
2017-10-05 15:36:32 +0330

    Initial Setup for the Project. Downloading Bootstrap and Preparing Initial Required Project Structure

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Online Shopping</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/jquery-ui.css">
+    <link rel="stylesheet" href="css/font-awesome.css">
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/jquery-color.js"></script>
+    <script type="text/javascript" src="js/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+  </head>
+  <body>
+    <div class="jumbotron">
+    </div>
+  </body>
+</html>
