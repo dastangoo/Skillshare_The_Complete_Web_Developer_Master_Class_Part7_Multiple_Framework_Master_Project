ad2da3f8722890bb75774d380bb581b665e6b7f6
dastangoo <hamed.dastangoo@gmail.com>
2017-10-05 20:44:26 +0330

    Creating proceed Modal

--- buy.php
+++ buy.php
@@ -7,7 +7,121 @@
   </head>
   <body>
     <?php include 'includes/header.php' ?>
-    <div class="container"></div>
+    <div class="container">
+      <div class="page-header">
+        <h2 class="pull-left">Checkout</h2>
+        <div class="pull-right">
+          <button class="btn btn-success" data-target="#proceed_modal" data-toggle="modal" data-backdrop="static" data-keyboard="false">Proceed</button>
+        </div>
+        <div class="modal fade" id="proceed_modal" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+              </div>
+              <div class="modal-body">
+                <form>
+                  <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" class="form-control" placeholder="Full Name" id="name">
+                  </div>
+                  <div class="form-group">
+                    <label for="email">Email Address</label>
+                    <input type="email" class="form-control" placeholder="Email" id="email">
+                  </div>
+                  <div class="form-group">
+                    <label for="contact">Contact Number</label>
+                    <input type="text" class="form-control" placeholder="Contact Number" id="contact">
+                  </div>
+                  <div class="form-group">
+                    <label for="contact">City</label>
+                    <input list="states" class="form-control">
+                    <datalist id="states">
+                      <option>Washington</option>
+                      <option>New York</option>
+                      <option>Florida</option>
+                      <option>Kansas</option>
+                      <option>Nebraska</option>
+                      <option>Oregon</option>
+                      <option>Indiana</option>
+                    </datalist>
+                  </div>
+                  <div class="form-group">
+                    <label for="address">Delivery Address</label>
+                    <textarea id="address" class="form-control"></textarea>
+                  </div>
+                  <div class="form-group">
+                    <input type="submit" class="btn btn-danger form-control">
+                  </div>
+
+                </form>
+              </div>
+              <div class="modal-footer">
+                <button class="btn btn-default" data-dismiss="modal">Close</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="clearfix"></div>
+      </div>
+      <div class="panel panel-default">
+        <div class="panel-heading">Order Detail</div>
+        <div class="panel-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th>S.no</th>
+                <th>Item</th>
+                <th>qty</th>
+                <th width="5%">Delete</th>
+                <th class="text-right">Price</th>
+                <th class="text-right">Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Beautiful Watch</td>
+                <td>1</td>
+                <td><button class="btn btn-danger btn-sm">Delete</button></td>
+                <td class="text-right"><b>100/=</b></td>
+                <td class="text-right"><b>100/=</b></td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>Teac Cup set</td>
+                <td>4</td>
+                <td><button class="btn btn-danger btn-sm">Delete</button></td>
+                <td class="text-right"><b>150/=</b></td>
+                <td class="text-right"><b>600/=</b></td>
+              </tr>
+            </tbody>
+          </table>
+          <table class="table">
+            <thead>
+              <tr>
+                <th colspan="2" class="text-center">Order Summary</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Subtotal</td>
+                <td class="text-right"><b>700/=</b></td>
+              </tr>
+              <tr>
+                <td>Delivery Charges</td>
+                <td class="text-right"><b>Free</b></td>
+              </tr>
+              <tr>
+                <td>Grand Total</td>
+                <td class="text-right"><b>700/=</b></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <br><br><br><br>
     <?php include 'includes/footer.php' ?>
   </body>
 </html>
