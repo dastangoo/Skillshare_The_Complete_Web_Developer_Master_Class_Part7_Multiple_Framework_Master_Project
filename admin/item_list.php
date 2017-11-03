cfcc6917d69702beca17641675145b3e1035d946
Hamed Dastangoo <hamed.dastangoo@gmail.com>
2017-11-03 21:39:18 +0330

    Applying PHP to the Submission and Validating the Form

--- admin/item_list.php
+++ admin/item_list.php
@@ -1,34 +1,38 @@
 <?php include '../includes/db.php';  
   if (isset($_POST['item_submit'])) {
     
-    mysqli_real_escape_string($conn, strip_tags($_POST['item_name']));
-    mysqli_real_escape_string($conn, strip_tags($_POST['item_description']));
-    mysqli_real_escape_string($conn, strip_tags($_POST['item_category']));
-    mysqli_real_escape_string($conn, strip_tags($_POST['item_quantity']));
-    mysqli_real_escape_string($conn, strip_tas($_POST['item_cost']));
-    mysqli_real_escape_string($conn, strip_tags($_POST['item_price']));
-    mysqli_real_escape_string($conn, strip_tags($_POST['item_discount']));
-    mysqli_real_escape_string($conn, strip_tags($_POST['item_delivery']));
+    $item_name = mysqli_real_escape_string($conn, strip_tags($_POST['item_name']));
+    $item_description = mysqli_real_escape_string($conn, $_POST['item_description']);
+    $item_category = mysqli_real_escape_string($conn, strip_tags($_POST['item_category']));
+    $item_quantity = mysqli_real_escape_string($conn, strip_tags($_POST['item_quantity']));
+    $item_cost = mysqli_real_escape_string($conn, strip_tags($_POST['item_cost']));
+    $item_price = mysqli_real_escape_string($conn, strip_tags($_POST['item_price']));
+    $item_discount = mysqli_real_escape_string($conn, strip_tags($_POST['item_discount']));
+    $item_delivery = mysqli_real_escape_string($conn, strip_tags($_POST['item_delivery']));
     
     if (isset($_FILES['item_image']['name'])) {
       $file_name = $_FILES['item_image']['name'];
       $path_address = "../images/items/$file_name";
+      $path_address_db = "images/items/$file_name";
       $img_confirm = 1;
-      $file_type = pathinfo($file_name);
+      $file_type = pathinfo($_FILES['item_image']['name'], PATHINFO_EXTENSION);
       if ($_FILES['item_image']['size'] > 200000) {
         $img_confirm = 0;
+        echo 'The size is very big!';
       }
       if ($file_type != 'jpg' && $file_type != 'png' && $file_type != 'gif') {
         $img_confirm = 0;
+        echo 'Type is not matching!';
       }
-      if ($img_confirm == 1) {
+      if ($img_confirm == 0) {
+        
+      } else {
         if (move_uploaded_file($_FILES['item_image']['tmp_name'], $path_address)) {
-          $item_ins_sql = "INSERT INTO items () VALUES ()";
+          $item_ins_sql = "INSERT INTO items (item_image, item_title, item_description, item_cat, item_qty, item_cost, item_price, item_discount, item_delivery) VALUES ('$path_address_db', '$item_name', '$item_description', '$item_category', '$item_quantity', '$item_cost', '$item_price', '$item_discount', '$item_delivery')";
+          mysqli_query($conn, $item_ins_sql);
         }
       }
     }
-    $ins_sql = "INSERT INTO items (item_image, item_title, item_description, item_cat, item_qty, item_cost, item_price, item_discount, item_delivery) VALUES ('$_POST[item_image]', '$_POST[item_name]', '$_POST[item_description]', '$_POST[item_category]', '$_POST[item_quantity]', '$_POST[item_cost]', '$_POST[item_price])','$_POST[item_discount]', '$_POST[item_delivery]'";
-    mysqli_query($conn, $ins_sql);
   }
 ?>
 <!DOCTYPE html>
@@ -38,8 +42,8 @@
     <title>Items List | Admin Panel</title>
     <link rel="stylesheet" href="../css/bootstrap.css">
     <link rel="stylesheet" href="../css/admin-style.css">
-    <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
-    <script>tinymce.init({ selector:'textarea' });</script>
+    <!-- <script src="https://cloud.tinymce.com/stable/tinymce.min.js"></script>
+    <script>tinymce.init({ selector:'textarea' });</script> -->
     <style media="screen">
     </style>
     <script src="../js/jquery.js"></script>
@@ -73,19 +77,19 @@
               <form method="post" enctype="multipart/form-data">
                 <div class="form-group">
                   <label for="">Item Image</label>
-                  <input type="file" name="item_image " value="" class="form-control">
+                  <input type="file" name="item_image" value="" class="form-control" required>
                 </div>
                 <div class="form-group">
                   <label for="">Item Name</label>
-                  <input type="text" class="form-control" id="" placeholder="" name="item_name">
+                  <input type="text" class="form-control" id="" placeholder="" name="item_name" required>
                 </div>
                 <div class="form-group">
                   <label for="">Item Description</label>
-                  <textarea rows="6" cols="80" class="form-control" name="item_description"></textarea>
+                  <textarea rows="6" cols="80" class="form-control" name="item_description" required></textarea>
                 </div>
                 <div class="form-group">
                   <label for="">Item Catgory</label>
-                  <select class="" name="item_category" class="form-control">
+                  <select class="" name="item_category" class="form-control" required>
                     <option value="">Select a Category</option>
                     <?php 
                       $cat_sql = "SELECT * FROM item_cat";
@@ -106,19 +110,19 @@
                 </div>
                 <div class="form-group">
                   <label for="">Item Quantity</label>
-                  <input type="number" class="form-control" id="" placeholder="" min="1" name="item_quantity">
+                  <input type="number" class="form-control" id="" placeholder="" min="1" name="item_quantity" required>
                 </div>
                 <div class="form-group">
                   <label for="">Item Cost</label>
-                  <input type="number" class="form-control" id="" placeholder="" min="1" name="item_cost">
+                  <input type="number" class="form-control" id="" placeholder="" min="1" name="item_cost" required>
                 </div>
                 <div class="form-group">
                   <label for="">Item Price</label>
-                  <input type="number" class="form-control" id="" placeholder="" min="1" name="item_price">
+                  <input type="number" class="form-control" id="" placeholder="" min="1" name="item_price" required>
                 </div>
                 <div class="form-group">
                   <label for="">Item Discount</label>
-                  <input type="text" class="form-control" id="" placeholder="" name="item_discount">
+                  <input type="text" class="form-control" id="" placeholder="" name="item_discount" required>
                 </div>
                 <div class="form-group">
                   <label for="">Item Delivery</label>
