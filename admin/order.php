a1368e116cbfa4f19c9c5348807c58e661656f5e
dastangoo <hamed.dastangoo@gmail.com>
2017-11-09 18:42:01 +0330

    Making Order Status Changer

--- admin/order.php
+++ admin/order.php
@@ -19,6 +19,21 @@
         xmlhttp.open('GET', 'order_list_process.php', true);
         xmlhttp.send();
       }
+      
+      function order_status(order_status, order_id) {
+        if (order_status == 1) {
+          order_status = 0
+        } else {
+          order_status = 1;
+        }
+        xmlhttp.onreadystatechange = function () {
+          if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            document.getElementById('get_order_list_data').innerHTML = xmlhttp.responseText;
+          }
+        }
+        xmlhttp.open('GET', 'order_list_process.php?order_status=' + order_status + '&order_id=' + order_id, true);
+        xmlhttp.send();        
+      }
     </script>
   </head>
   <body onload="get_order_list();">
--- admin/order_list_process.php
+++ admin/order_list_process.php
@@ -1,3 +1,9 @@
+<?php include '../includes/db.php';
+  if (isset($_REQUEST['order_status'])) {
+    $up_sql = "UPDATE orders SET order_status='$_REQUEST[order_status]' WHERE order_id = '$_REQUEST[order_id]'";
+    mysqli_query($conn, $up_sql);
+  }
+?>
 <table class="table table-bordered table-striped table-hover">
   <thead class="item-head">
     <tr>
@@ -13,16 +19,33 @@
     </tr>
   </thead>
   <tbody>
-    <tr>
-      <td>1</td>
-      <td>John</td>
-      <td>Doe</th>
-      <td>123456789</td>
-      <td>Arizona</td>
-      <td>No. 1234, Main Street, Pheonix</td>
-      <td>79274932</td>
-      <td class="text-right">430/=</td>
-      <td class="text-center"><button class="btn btn-warning btn-block btn-sm">Pending</button></td>
-    </tr>
+    <?php 
+      $sql = "SELECT * FROM orders";
+      $run = mysqli_query($conn, $sql);
+      $c = 1;
+      while ($rows = mysqli_fetch_assoc($run)) {
+        if ($rows['order_status'] == 0) {
+          $btn_class = 'btn-warning';
+          $btn_value = 'Pending';
+        } else {
+          $btn_class = 'btn-success';
+          $btn_value = 'Sent';
+        }
+        echo "
+          <tr>
+            <td>$c</td>
+            <td>$rows[order_name]</td>
+            <td>$rows[order_email]</th>
+            <td>$rows[order_contact]</td>
+            <td>$rows[order_state]</td>
+            <td>$rows[order_delivery_address]</td>
+            <td>$rows[order_checkout_ref]</td>
+            <td class='text-right'>$rows[order_total]</td>"; ?>
+            <td class='text-center'><button class='btn <?php echo $btn_class ?> btn-block btn-sm' onclick="order_status(<?php echo $rows['order_status'] ?>, <?php echo $rows['order_id'] ?>);"><?php echo $btn_value ?></button></td>
+          </tr>        
+          <?php 
+          $c++;
+      }
+    ?>
   </tbody>
 </table>
